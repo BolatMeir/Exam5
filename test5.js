161f41f072ae67b8087b21deb51d69ebc98ef7b3
Meiir <a12790958@gmail.com>
2024-11-25 20:04:24 +0500

    Exam 5 finished

---
+++ test5.js
@@ -0,0 +1,80 @@
+////Task 1
+
+
+// while(true){
+//     let a = +prompt("Birinshi san engiz!")
+//     let b = +prompt("Ekinshi san engiz!")
+//     function операцияны(a,b){
+//         return a +b
+//     }
+//     function операцияны(a,b){
+//         return a - b
+//     }
+//     function операцияны(a,b){
+//         return a * b
+//     }
+//     function операцияны(a,b){
+//         return a / b
+//     }
+//     alert(операцияны(a,b))
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////Task 2
+
+
+// while(true){
+// let baga = +prompt("Zhattin bagasy:")
+//     if(baga >=5000 && baga < 10000){
+//         alert(`Sizde ${baga*0.9} shikti zhenildikpen`)
+//     }else if(baga >= 10000 && baga < 20000){
+//         alert(`Sizde ${baga*0.8} shikti zhenildikpen`)
+//     }else if(baga >=20000){
+//         alert(`Sizde ${baga*0.7} shikti zhenildikpen`)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+////Task 3
+
+
+
+
+
+// while(true){
+//     let san = +prompt("San engiz")
+//     if(san == 49){
+//         alert("Duris taptin!")
+//         break;
+//     }else if(san >= 44 && san <49){
+//         alert("Сіздің жасырылған саныңыз Сәл төмен")
+//     }else if(san <= 54 && san >49){
+//         alert("Сіздің жасырылған саныңыз Сәл жоғары")
+//     }else if(san < 44 && san >=1){
+//         alert("Сіздің жасырылған саныңыз Жай төмен")
+//     }else if(san > 54 && san <100){
+//         alert( "Сіздің жасырылған саныңыз Жай жоғары")
+// }
+// }
