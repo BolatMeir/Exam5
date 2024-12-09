5f8a655157c56b392a4e9f2b9033ca6cf1a1361d
Meiir <a12790958@gmail.com>
2024-12-09 19:53:25 +0500

    new commit

--- test5.js
+++ test5.js
@@ -1,104 +1,3 @@
-////Task 1
-
-
-// while(true){
-//     let a = +prompt("Birinshi san engiz!")
-//     let b = +prompt("Ekinshi san engiz!")
-//     function операцияны(a,b){
-//         return a +b
-//     }
-//     function операцияны(a,b){
-//         return a - b
-//     }
-//     function операцияны(a,b){
-//         return a * b
-//     }
-//     function операцияны(a,b){
-//         return a / b
-//     }
-//     alert(операцияны(a,b))
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-////Task 2
-
-
-// while(true){
-// let baga = +prompt("Zhattin bagasy:")
-//     if(baga >=5000 && baga < 10000){
-//         alert(`Sizde ${baga*0.9} shikti zhenildikpen`)
-//     }else if(baga >= 10000 && baga < 20000){
-//         alert(`Sizde ${baga*0.8} shikti zhenildikpen`)
-//     }else if(baga >=20000){
-//         alert(`Sizde ${baga*0.7} shikti zhenildikpen`)
-//     }
-// }
-
-
-
-
-
-
-
-
-
-////Task 3
-
-
-
-
-
-// while(true){
-//     let san = +prompt("San engiz")
-//     if(san == 49){
-//         alert("Duris taptin!")
-//         break;
-//     }else if(san >= 44 && san <49){
-//         alert("Сіздің жасырылған саныңыз Сәл төмен")
-//     }else if(san <= 54 && san >49){
-//         alert("Сіздің жасырылған саныңыз Сәл жоғары")
-//     }else if(san < 44 && san >=1){
-//         alert("Сіздің жасырылған саныңыз Жай төмен")
-//     }else if(san > 54 && san <100){
-//         alert( "Сіздің жасырылған саныңыз Жай жоғары")
-// }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //////////// Date: 09.12.2024     Exam 6
 
 ////Taks 1
@@ -390,53 +289,53 @@
 
 
 
-let School = [
+// let School = [
     
-    student = [
-         {
-             Schoolname: 'Amjilt Cyber School',
-             phoneNUmber: '+77077077070',
-             studentName: 'Meir',
-             top: '24/8',
-             progress: '60',
-             lessons: 'Math, Playing, Coding, Programming'
-         },
-         {
-             Schoolname: 'Amjilt Cyber School',
-             phoneNUmber: '+77088088080',
-             studentName: 'Yerkebulan',
-             top: '24/8',
-             progress: '100',
-             lessons: 'Coding, Programming, Math, Reading'
-         },
-         {
-             Schoolname: 'Amjilt Cyber School',
-             phoneNUmber: '+77099099090',
-             studentName: 'Bakbergen',
-             top: '24/7',
-             progress: '60',
-             lessons: 'Coding, Programming, '
-         },
-         {
-             Schoolname: 'Amjilt Cyber School',
-             phoneNUmber: '+7101011010',
-             studentName: 'Dosym',
-             top: '24/10',
-             progress: '80',
-             lessons: 'Reading, Coding, Programming'
-         },
-         {
-             Schoolname: 'Amjilt Cyber School',
-             phoneNUmber: '+77022022020',
-             studentName: 'Kairat Nurtas',
-             top: '24/7',
-             progress: '35',
-             lessons: 'Singing, Coding'
-         },
-     ], 
-     function applyDecreaseToTop(top){
-            let top_aty = prompt('Top atyn jaz')
-            let payiz = +prompt('Qansha payiz tomendetu kerek?')
-     },
-]
+//     student = [
+//          {
+//              Schoolname: 'Amjilt Cyber School',
+//              phoneNUmber: '+77077077070',
+//              studentName: 'Meir',
+//              top: '24/8',
+//              progress: '60',
+//              lessons: 'Math, Playing, Coding, Programming'
+//          },
+//          {
+//              Schoolname: 'Amjilt Cyber School',
+//              phoneNUmber: '+77088088080',
+//              studentName: 'Yerkebulan',
+//              top: '24/8',
+//              progress: '100',
+//              lessons: 'Coding, Programming, Math, Reading'
+//          },
+//          {
+//              Schoolname: 'Amjilt Cyber School',
+//              phoneNUmber: '+77099099090',
+//              studentName: 'Bakbergen',
+//              top: '24/7',
+//              progress: '60',
+//              lessons: 'Coding, Programming, '
+//          },
+//          {
+//              Schoolname: 'Amjilt Cyber School',
+//              phoneNUmber: '+7101011010',
+//              studentName: 'Dosym',
+//              top: '24/10',
+//              progress: '80',
+//              lessons: 'Reading, Coding, Programming'
+//          },
+//          {
+//              Schoolname: 'Amjilt Cyber School',
+//              phoneNUmber: '+77022022020',
+//              studentName: 'Kairat Nurtas',
+//              top: '24/7',
+//              progress: '35',
+//              lessons: 'Singing, Coding'
+//          },
+//      ], 
+//      function applyDecreaseToTop(top){
+//             let top_aty = prompt('Top atyn jaz')
+//             let payiz = +prompt('Qansha payiz tomendetu kerek?')
+//      },
+// ]
 
